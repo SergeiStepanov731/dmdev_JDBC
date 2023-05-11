7a599aafc807889a2f990e68bba87ccfdb251cc3
SergeiStepanov731 <iffymailone@gmail.com>
2023-05-11 13:38:11 +0300

    Initial commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+b
+2src/main/java/org/example/util/PropertiesUtil.java,d\9\d9da1e91797e8f167c7e7f31d28d3b2febeaf3ab
---
+++ src/main/java/org/example/util/ConnectionManager.java
@@ -0,0 +1,51 @@
+package org.example.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+
+
+    static {
+        loadDriver();
+    }
+
+    private ConnectionManager() {
+    }
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+/*    String password = "root";
+      String username = "postgres";
+      String url = "jdbc:postgresql://localhost:5432/flight_repository";
+      try (Connection connection = DriverManager.getConnection(url, username, password)) {
+           System.out.println(connection.getTransactionIsolation());
+   }
+*/
+
+
+
